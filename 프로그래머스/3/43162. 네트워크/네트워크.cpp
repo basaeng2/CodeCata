92faafeaba3b49ad5f8e17b1c54a47f1dd00300a
arbasang <154686862+arbasang@users.noreply.github.com>
2025-01-02 12:30:23 +0900

    [level 3] Title: 네트워크, Time: 0.05 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,31 +1,31 @@
-#include <string>
 #include <vector>
-
+#include <queue>
 using namespace std;
-
-void DFS(int node, vector<vector<int>>& computers, vector<bool>& visited)
-{
-    visited[node] = true;
-    for(int i = 0; i< computers.size(); i++)
-    {
-        if(computers[node][i] == 1 && !visited[i])
-        {
-            DFS(i, computers, visited);
-        }
-    }
-}
 
 int solution(int n, vector<vector<int>> computers) {
     vector<bool> visited(n, false);
     int answer = 0;
-    for(int i = 0; i < n; i++)
-    {
-        if(!visited[i])
-        {
-            DFS(i, computers, visited);
-            answer++;
+
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            answer++; 
+            queue<int> q;
+            q.push(i);
+            visited[i] = true;
+            
+            while (!q.empty()) {
+                int node = q.front();
+                q.pop();
+
+                for (int j = 0; j < n; j++) {
+                    if (computers[node][j] == 1 && !visited[j]) {                       
+                        q.push(j);
+                        visited[j] = true;
+                    }
+                }
+            }
         }
     }
-    
+
     return answer;
 }
