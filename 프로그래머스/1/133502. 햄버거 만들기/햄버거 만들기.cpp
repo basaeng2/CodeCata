10dff31f581774a05d39ffdf9417d5cb722432ab
arbasang <154686862+arbasang@users.noreply.github.com>
2025-01-07 15:20:05 +0900

    [level 1] Title: 햄버거 만들기, Time: 15.04 ms, Memory: 38.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> ingredient) {
+    int answer = 0;
+    vector<int> stack;
+    
+    for(int item : ingredient)
+    {
+        stack.push_back(item);
+        
+        if(stack.size() >= 4 &&
+            stack[stack.size() - 4] == 1 &&
+            stack[stack.size() - 3] == 2 &&
+            stack[stack.size() - 2] == 3 &&
+            stack[stack.size() - 1] == 1)
+        {
+            for(int i = 0; i < 4; i++)
+            stack.pop_back();
+            
+            answer++;
+        }
+    }    
+    return answer;
+}
