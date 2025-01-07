148cd554c65bf0253a37b604b0f0fb596b60ef24
arbasang <154686862+arbasang@users.noreply.github.com>
2025-01-07 12:54:11 +0900

    [level 1] Title: 둘만의 암호, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string s, string skip, int index) {
+    string answer = "";
+    vector<char> Alphabet;
+
+    sort(skip.begin(), skip.end());
+        
+    for(int i = 0; i < 26; i ++)
+    {
+        char ch = 'a' + i;
+        if(skip.find(ch) == string::npos)
+         Alphabet.push_back(ch);
+    }
+    
+    for(char ch : s)
+    {
+        int pos = find(Alphabet.begin(),Alphabet.end(), ch) - Alphabet.begin();
+        pos = (pos + index) % Alphabet.size();
+        answer += Alphabet[pos];
+    }
+    
+    return answer;
+}
