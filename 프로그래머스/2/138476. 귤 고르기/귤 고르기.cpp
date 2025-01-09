6455b809dbfd35d2148781c854f2597241632e95
arbasang <154686862+arbasang@users.noreply.github.com>
2025-01-09 09:15:40 +0900

    [level 2] Title: 귤 고르기, Time: 31.09 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
@@ -7,27 +7,28 @@ using namespace std;
 
 int solution(int k, vector<int> tangerine) {
     int answer = 0;
-
+    
     map<int, int> size_map;
-    for (int size : tangerine) {
+    
+    for(int size : tangerine)
+    {
         size_map[size]++;
     }
-
+    
     vector<int> counts;
-    for (auto &[size, count] : size_map) {
-        counts.push_back(count);
+    for(auto& [size, count] : size_map)
+    {
+       counts.push_back(count);
     }
     
     sort(counts.rbegin(), counts.rend());
     
     int selected = 0;
-    for (int count : counts) {
+    for(int count : counts)
+    {
         selected += count;
         answer++;
-        if (selected >= k) {
-            break;
-        }
+        if(selected >= k) break;
     }
-
     return answer;
 }
