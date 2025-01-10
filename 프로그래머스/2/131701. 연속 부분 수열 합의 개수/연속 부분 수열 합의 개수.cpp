529d58fc4309bfbaeb5744293605163570b91b8b
arbasang <154686862+arbasang@users.noreply.github.com>
2025-01-10 09:37:32 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 196.00 ms, Memory: 23.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <set>
+#include <queue>
+using namespace std;
+
+int solution(vector<int> elements) {
+    set<int> uniqueSums;
+    int n = elements.size();
+    
+    vector<int> circular(elements.begin(), elements.end());
+    circular.insert(circular.end(), elements.begin(), elements.end());
+    
+    for (int length = 1; length <= n; ++length) {
+        for (int start = 0; start < n; ++start) {
+            int sum = 0;
+            for (int i = 0; i < length; ++i) {
+                sum += circular[start + i];
+            }
+            uniqueSums.insert(sum);
+        }
+    }
+    
+    return uniqueSums.size();
+}
