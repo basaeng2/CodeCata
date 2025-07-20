a0b42633b7b5839d8352d62a923f4f2b67aa954c
Jaehyeon <154686862+basaeng2@users.noreply.github.com>
2025-07-20 22:58:21 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 4.11 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -9,10 +9,14 @@ int solution(vector<int> citations) {
     
     // H-Index 계산
     int h_index = 0; // H-Index 초기화
-    for (int i = 0; i < citations.size(); ++i) {
-        if (citations[i] >= i + 1) {
+    for (int i = 0; i < citations.size(); ++i)
+    {
+        if (citations[i] >= i + 1)
+        {
             h_index = i + 1; // 조건을 만족하면 h_index를 갱신
-        } else {
+        }
+        else
+        {
             break; // 조건을 만족하지 않으면 반복 종료
         }
     }
